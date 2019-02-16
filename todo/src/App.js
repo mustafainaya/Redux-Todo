956bdb6aafcd508d64d423833728e414dacc1a3a
mustafa inaya <moose.inaya@icloud.com>
2019-02-16 17:41:24 -0200

    DEBUGG, Debugging my title .
    I had an issue once I implemented local storage .

--- todo/src/App.js
+++ todo/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<h1>{this.props.appTitle}</h1>
+				<h1>{this.props.appTitle} Redux Todo List :)</h1>
 				<TodoList />
 				<TodoInput />
 			</div>
